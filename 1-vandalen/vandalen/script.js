827b6a210ec614a1ea0e4e848fe5e5e35d51ab86
as223jc <as223jc@student.lnu.se>
2013-11-20 23:20:34 +0100

    färdiga lösningen för 1.4

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,31 @@
 "use strict";
 
 var makePerson = function(persArr){
+	var result = {};
+	var ages = [];
+	var names = [];
+	var avgAge = 0;
+	var namn = "";
 
+	for (var i = 0; i < persArr.length; i++) {
+		ages[i] = persArr[i].age;
+		avgAge += persArr[i].age;
+		names[i] = persArr[i].name;
+	};	
+	
+	names.sort();
+	for (var i = 0; i < names.length; i++) {
+		namn += names[i];
+		if(i < names.length - 1)
+			namn += ", ";
+	};
+	
+	avgAge = avgAge / persArr.length;
+	
+	avgAge = Math.round(avgAge);
+	ages.sort();
 
-	// Din kod här...
+	result = {minAge : ages[0], maxAge : ages[persArr.length-1], averageAge : avgAge, names : namn};
 
+	return result;
 }
-
