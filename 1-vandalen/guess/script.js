4fc9c886b4a1ceaa534c73dbb339ca8584878eb6
as223jc <as223jc@student.lnu.se>
2013-11-20 16:22:05 +0100

    färdiga lösningen för uppgift 1.1

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,24 +2,34 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor((Math.random()*100)+1);
+	var NumberOfGuesses = 0;
 	
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
-		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
-		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+		try{
+			console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
+			NumberOfGuesses++;
+			if(isNaN(number) || number === "") 
+				throw "Du kan enbart ange siffror.";
+			if(number>100 || number<0)
+				return [false, "Talet är utanför intervallet 0 - 100"];	
+			if(number==secret)
+				return [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + NumberOfGuesses + " gissningar för att hitta det."];
+			if(number<secret)
+				return [false, "Det hemliga talet är högre!"];
+			if(number>secret)
+				return [false, "Det hemliga talet är lägre!"];	
+			}
+			catch (err)
+			 {
+			 	return [false, err];
+			 }
 
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
 	};
 	
 	// ------------------------------------------------------------------------------
+
 
 
 
