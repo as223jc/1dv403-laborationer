1207e6a27b4b85d2de60f26fbce90faf556d46ca
OpenShift guest <as223jc@student.lnu.se>
2013-11-21 09:19:23 -0500

    uppdatering

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -11,21 +11,32 @@ var makePerson = function(persArr){
 		ages[i] = persArr[i].age;
 		avgAge += persArr[i].age;
 		names[i] = persArr[i].name;
-	};	
+	}	
 	
-	names.sort();
-	for (var i = 0; i < names.length; i++) {
+	result.names = names.sort(function(a, b){
+        return a.localeCompare(b)});
+        
+	for (i = 0; i < names.length; i++) {
 		namn += names[i];
 		if(i < names.length - 1)
 			namn += ", ";
-	};
+	}
 	
 	avgAge = avgAge / persArr.length;
 	
 	avgAge = Math.round(avgAge);
 	ages.sort();
-
+	
+   
+        
 	result = {minAge : ages[0], maxAge : ages[persArr.length-1], averageAge : avgAge, names : namn};
-
+    
 	return result;
-}
+};
+
+
+var data = [{name: "Öohn Häggerud", age: 37}, {name: "Åohan Leitet", age: 36}, {name: "Äats Loock", age: 46}];
+
+var result = makePerson(data);
+
+console.log(result);
